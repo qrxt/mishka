d6b167ed71f605e62b68f2b2f5de9db8986e9249
qrxt <46269438+qrxt@users.noreply.github.com>
2019-04-23 02:22:54 +0300

    Начало бургерного меню

--- source/js/script.js
+++ source/js/script.js
@@ -3,17 +3,17 @@ const mainNavToggle = mainNav.querySelector(".main-nav__toggle");
 
 mainNav.classList.remove("main-nav--nojs");
 
-mainNavToggle.addEventListener("click", function () {
-  /*
-  if (mainNavToggle.classList.contains("main-nav__toggle--closed")) {
-    mainNavToggle.setAttribute("aria-label", "Закрыть меню навигации");
-  } else {
-    mainNavToggle.setAttribute("aria-label", "Открыть меню навигации");
-  }
-  */
+mainNavToggle.addEventListener("click", function() {
+    /*
+    if (mainNavToggle.classList.contains("main-nav__toggle--closed")) {
+      mainNavToggle.setAttribute("aria-label", "Закрыть меню навигации");
+    } else {
+      mainNavToggle.setAttribute("aria-label", "Открыть меню навигации");
+    }
+    */
 
-  mainNavToggle.classList.toggle("main-nav__toggle--closed");
-  mainNavToggle.classList.toggle("main-nav__toggle--opened");
+    mainNavToggle.classList.toggle("main-nav__toggle--closed");
+    mainNavToggle.classList.toggle("main-nav__toggle--opened");
 
-  mainNav.classList.toggle("main-nav--opened");
+    mainNav.classList.toggle("main-nav--opened");
 });
