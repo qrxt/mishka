d90722b9063c4a9c12793b5836920252041279a6
qrxt <qrx357@gmail.com>
2019-04-22 23:28:51 +0300

    Правки стилизации

--- source/js/script.js
+++ source/js/script.js
@@ -1,18 +1,19 @@
 const mainNav = document.querySelector(".main-nav");
-const mainNavToggle = document.querySelector(".main-nav__toggle");
+const mainNavToggle = mainNav.querySelector(".main-nav__toggle");
 
 mainNav.classList.remove("main-nav--nojs");
 
 mainNavToggle.addEventListener("click", function () {
+  /*
   if (mainNavToggle.classList.contains("main-nav__toggle--closed")) {
     mainNavToggle.setAttribute("aria-label", "Закрыть меню навигации");
   } else {
     mainNavToggle.setAttribute("aria-label", "Открыть меню навигации");
   }
+  */
 
   mainNavToggle.classList.toggle("main-nav__toggle--closed");
   mainNavToggle.classList.toggle("main-nav__toggle--opened");
 
-  mainNav.classList.toggle("main-nav--closed");
   mainNav.classList.toggle("main-nav--opened");
 });
