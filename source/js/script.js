b2f81df591be258c99a57117e387cb37093e5449
qrxt <qrx357@gmail.com>
2019-04-23 14:19:31 +0300

    Бургерное меню в шапке

--- source/js/script.js
+++ source/js/script.js
@@ -3,17 +3,13 @@ const mainNavToggle = mainNav.querySelector(".main-nav__toggle");
 
 mainNav.classList.remove("main-nav--nojs");
 
-mainNavToggle.addEventListener("click", function() {
-    /*
-    if (mainNavToggle.classList.contains("main-nav__toggle--closed")) {
-      mainNavToggle.setAttribute("aria-label", "Закрыть меню навигации");
-    } else {
-      mainNavToggle.setAttribute("aria-label", "Открыть меню навигации");
-    }
-    */
+mainNavToggle.addEventListener("click", function () {
+  if (mainNavToggle.classList.contains("burger-menu--opened")) {
+    mainNavToggle.setAttribute("aria-label", "Открыть меню навигации");
+  } else {
+    mainNavToggle.setAttribute("aria-label", "Закрыть меню навигации");
+  }
 
-    mainNavToggle.classList.toggle("main-nav__toggle--closed");
-    mainNavToggle.classList.toggle("main-nav__toggle--opened");
-
-    mainNav.classList.toggle("main-nav--opened");
+  mainNavToggle.classList.toggle("burger-menu--opened");
+  mainNav.classList.toggle("main-nav--opened");
 });
