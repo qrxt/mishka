94eceaaf2f254ac4b2fe938ae37ed0aca9f1a460
qrxt <qrx357@gmail.com>
2019-04-30 23:09:22 +0300

    Оптимизация изображений

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@ var server = require("browser-sync").create();
 const csso = require("gulp-csso");
 const rename = require("gulp-rename");
 
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp");
+
+const svgstore = require("gulp-svgstore");
+
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
     .pipe(plumber())
@@ -37,6 +42,31 @@ gulp.task("server", function () {
 
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/*.html").on("change", server.reload);
+});
+
+gulp.task("optimages", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(imagemin([
+      imagemin.optipng({ optimizationLevel: 3 }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({ quality: 90 }))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("sprite", function () {
+  return gulp.src("source/img/icon-*.svg")
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
